70d4271ae9655f8881a864ed9cd32635782f4700
Kaznacheev Pavel <kaznacheev9@yandex.ru>
2025-07-08 15:44:50 +0300

    refactor: refactor jwt auth filter

--- auth-service/src/main/java/ru/kaznacheev/wallet/authservice/config/filter/JwtAuthenticationFilter.java
+++ auth-service/src/main/java/ru/kaznacheev/wallet/authservice/config/filter/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,22 +22,23 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final String HEADER_TITLE = "Authorization";
     private final String TOKEN_PREFIX = "Bearer ";
+    private final String JTI_CLAIM = "jti";
 
     private final JwtService jwtService;
     private final AccessBlacklistService accessBlacklistService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader(HEADER_TITLE);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (authHeader == null || !authHeader.startsWith(TOKEN_PREFIX)) {
             filterChain.doFilter(request, response);
             return;
         }
         String token = authHeader.replace(TOKEN_PREFIX, "");
         DecodedJWT decodedJWT = jwtService.verify(token);
-        accessBlacklistService.verify(decodedJWT.getClaim("jti").asString());
+        accessBlacklistService.verify(decodedJWT.getClaim(JTI_CLAIM).asString());
         Authentication authentication = new UsernamePasswordAuthenticationToken(decodedJWT.getSubject(), null,
                 Collections.emptyList());
         SecurityContextHolder.getContext().setAuthentication(authentication);
